e3131cbae85000e781d953a202fd194fb02cc721
Jesús Solis <jesolis@inteliglobe.com>
2019-05-02 14:26:23 -0500

    Comentarios en codigo

--- main.py
+++ main.py
@@ -5,15 +5,24 @@ from PIL import Image
 
 
 def main():
+    # Usando PIL abrimos la imagen y la guardamos en una variable
     img = Image.open('source/image1.png')
+    # Para trabjar con la imagen convertimos la imagen en un arreglo
+    # para manipular los pixeles
     img_array = np.array(img)
+    # Usando la funcion integral_image de skicit-image
+    # sumamos todos los cuadrantes de la imagen
     my_intergral_image = integral_image(img_array)
+    # Para poder mostrar la imagen debemos realizar
+    # la conversión del arreglo a una imagen
+    # transformando los datos del arreglo a pixeles nuevamente
+    # usando el tipo de dato uint8 de numpy
     image_real_integral = Image.fromarray(
         np.uint8(my_intergral_image)
     )
-    # Imagen real
+    # Mostramos la primera imagen
     img.show()
-    # Imagen Integral
+    # Finalmente mostramos la Imagen Integral generada
     image_real_integral.show()
     # image_real_integral.save('image_integral_1.png')
 
